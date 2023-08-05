a5636a41b97fe4eb15248100a339b79b7e752a56
Nikhil Verma <63592210+nikhilverma17@users.noreply.github.com>
2023-08-05 17:46:57 +0530

    first

---
+++ src/Form.js
@@ -0,0 +1,11 @@
+import React from "react"
+function Form(){
+return(
+<form className="form">
+<input type="text" placeholder="username" />
+<input type="password" placeholder="password" />
+<button type="submit">Submit</button>
+</form>
+)
+}
+export default Form;
